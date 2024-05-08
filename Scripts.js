c4a5ce79a1e9c31a0ddcbf07b7b0a5de5656bb9c
furthestgoose <adamby20@gmail.com>
2024-05-08 14:07:25 +0100

    first commit

---
+++ Scripts.js
@@ -0,0 +1,84 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+gsap.registerPlugin(ScrollTrigger) 
+console.clear();
+gsap.set(".cardx", { position: "absolute" });
+
+// investor cards
+gsap.to(".sloganx:not(:last-child)", {
+  yPercent: -110,
+  stagger: 0.5,
+  scrollTrigger: {
+    trigger: ".__sloganCards",
+    markers: false,
+    start: "top top",
+    end: "+=900px",
+    scrub: 0.1,
+    pin: true,
+    invalidateOnRefresh: true
+  },
+  ease: "none"
+});
+
+const horizontalSections = gsap.utils.toArray('section.horizontal')
+
+horizontalSections.forEach(function (sec, i) {	
+  
+  var thisPinWrap = sec.querySelector('.pin-wrap');
+  var thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
+  
+  var getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth); 
+
+  gsap.fromTo(thisAnimWrap, { 
+    x: () => thisAnimWrap.classList.contains('to-right') ? 0 : getToValue() 
+  }, { 
+    x: () => thisAnimWrap.classList.contains('to-right') ? getToValue() : 0, 
+    ease: "none",
+    scrollTrigger: {
+      trigger: sec,		
+      start: "top top",
+      end: () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth),
+      pin: thisPinWrap,
+      invalidateOnRefresh: true,
+      //anticipatePin: 1,
+      scrub: true,
+      //markers: true,
+    }
+  });
+
+});	
+
+let footer = document.querySelector("footer"),
+    getOverlap = () => Math.min(window.innerHeight, footer.offsetHeight), // we never want it to overlap more than the height of the screen
+    adjustFooterOverlap = () => footer.style.marginTop = -getOverlap() + "px"; // adjusts the margin-top of the footer to overlap the proper amount
+
+adjustFooterOverlap();
+
+// to make it responsive, re-calculate the margin-top on the footer when the ScrollTriggers revert
+ScrollTrigger.addEventListener("revert", adjustFooterOverlap);
+
+// magic
+ScrollTrigger.create({
+  trigger: footer,
+  start: () => "top " + (window.innerHeight - getOverlap()),
+  end: () => "+=" + getOverlap(),
+  pin: true,
+});
+  
